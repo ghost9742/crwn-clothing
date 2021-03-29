c27684ce55769aba5f82c7ab15b09c671d575837
Nemanja <milosavljevic.nemanja87@gmail.com>
2021-03-29 12:16:02 +0200

    adding form components: custom button, form input and sign in form

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,1 +1,11 @@
-import "./sign-in-and-sign-up.scss";
+import "./sign-in-and-sign-up.styles.scss";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
